d95ef4893660cfcf509199ed8fb4d0380465ea88
ChameeraRupasinghe <chameera.15@cse.mrt.ac.lk>
2018-07-03 11:11:08 +0530

    Structure completed. CSS not done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { PriceDispalyComponent } from './price-dispaly/price-dispaly.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductRowComponent
+    ProductRowComponent,
+    ProductImageComponent,
+    PriceDispalyComponent,
+    ProductDepartmentComponent
   ],
   imports: [
     BrowserModule
